482a5ff41a091ca32e1332b4f30f20af9aa21fe1
ICEE93 <gsson93@gmail.com>
2019-05-07 06:13:55 +0200

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,9 +1,13 @@
 <?php 
-	$sql = @mysqli_connect("localhost", "root", "", "test") or die("lolripserver"); 
+	$host = "localhost";               // Värd
+	$user = "root";                    // User@mysql
+	$pass = "";                        // Pw
+	$name = "test"; 				   // Db
+	$conn = @mysqli_connect($host, $user, $pass, $name) or die("lolripserver"); 
 		/* Koppla upp eller dö och kasta felmeddelande */
-		mysqli_query($sql, "UPDATE visitors SET visitors = visitors + 1"); 
+		mysqli_query($conn, "UPDATE visitors SET visitors = visitors + 1"); 
 		/* Uppdatera tables i sql databasen för att öka antal besökare med + 1 */	
-	$count = mysqli_fetch_row(mysqli_query($sql, "SELECT visitors FROM visitors"));
+	$count = mysqli_fetch_row(mysqli_query($conn, "SELECT visitors FROM visitors"));
 		/* Hämta information från databasfältet visitors */
 		print "Visitors: $count[0]"; 
 		/* Printa i webbläsaren */
