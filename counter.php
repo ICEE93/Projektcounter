a51e06bae4b8f12bc238d507e22a99dcec61ddf1
ICEE93 <gsson93@gmail.com>
2019-05-07 06:02:50 +0200

    förenkling och förklaringar -II-

--- counter.php
+++ counter.php
@@ -1,6 +1,10 @@
 <?php 
-$sql = @mysqli_connect("localhost", "root", "", "test") or die("lolripserver"); //koppla upp eller dö och kasta felmeddelande
-		mysqli_query($sql, "UPDATE visitors SET visitors = visitors + 1"); //
-$count = mysqli_fetch_row(mysqli_query($sql, "SELECT visitors FROM visitors"));
+	$sql = @mysqli_connect("localhost", "root", "", "test") or die("lolripserver"); 
+		/* Koppla upp eller dö och kasta felmeddelande */
+		mysqli_query($sql, "UPDATE visitors SET visitors = visitors + 1"); 
+		/* Uppdatera tables i sql databasen för att öka antal besökare med + 1 */	
+	$count = mysqli_fetch_row(mysqli_query($sql, "SELECT visitors FROM visitors"));
+		/* Hämta information från databasfältet visitors */
 		print "Visitors: $count[0]"; 
+		/* Printa i webbläsaren */
 		?>
